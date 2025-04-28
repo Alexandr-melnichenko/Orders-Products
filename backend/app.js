5a9bb8e87df6f89cdac5c9a68a1f465bdd93476c
Alexandr Melnichenko <alexandr.melnichenko@gmail.com>
2025-04-28 23:15:53 +0300

    created config for dev and production docker containers

--- backend/app.js
+++ backend/app.js
@@ -10,13 +10,17 @@ const app = express();
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
-    credentials: true
+    credentials: true,
   })
 );
 app.use(express.json());
 app.use("/api", apiRouter);
+app.get("/api/test", (req, res) => {
+  console.log("Запрос получен!"); // Проверка в логах
+  res.send("API работает!");
+});
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
