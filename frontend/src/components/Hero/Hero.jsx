dc4b49a35a265cfaa30b180ada0bb106eb3dca26
Alexandr Melnichenko <alexandr.melnichenko@gmail.com>
2025-05-07 12:49:05 +0300

    change hero background image

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -3,8 +3,12 @@ import style from "./Hero.module.css";
 export const Hero = () => {
   return (
     <div className={style.heroWrapper}>
-      <h1 className={style.heroTitle}>INVENTORY</h1>
-      <h2 className={style.heroOffer}>Try our warehouse management system! </h2>
+      <div className={style.centralBlock}>
+        <h1 className={style.heroTitle}>INVENTORY</h1>
+        <h2 className={style.heroOffer}>
+          Try our warehouse management system!{" "}
+        </h2>
+      </div>
     </div>
   );
 };
